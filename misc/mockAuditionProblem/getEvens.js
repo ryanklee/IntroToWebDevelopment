75a7ee42717940031bbe9d2375a54553fc056aba
Ryan Kleeberger <rylklee@gmail.com>
2017-07-16 16:29:44 -0500

    SG mock audition

---
+++ misc/mockAuditionProblem/getEvens.js
@@ -0,0 +1,52 @@
+function notNumeric(startNum, endNum, stepNum){
+  
+  // check whether numeric,
+  // if not numeric, display error to user
+  
+}
+
+function notPos(stepNum){
+  
+  // check whether positive int,
+  // if not positive int, display error to user
+  
+}
+
+function lessThan(startNum, endNum){
+  
+  // check whether startNum < endNum
+  // if not <, display error to user
+  
+}
+
+function genInputArray(startNum, endNum){
+  var inputArray = new Array();
+  for (var i = startNum; i <= endNum; i++){
+    inputArray.push(i);
+  }
+  console.log(inputArray)
+  return inputArray
+}
+
+function genEvenArray(inputArray){
+  var evenArray = new Array();
+  for (var i = 0; i <= inputArray.length; i++){
+    if (inputArray[i] % 2 === 0) {
+      evenArray.push(inputArray[i]);
+    }
+  }
+  console.log(evenArray)
+  return evenArray
+}
+
+function genStepArray(inputArray, stepNum){
+  var stepArray = new Array();
+  for (var i = 0; i < inputArray.length; i++){
+    if (i % stepNum === 0){
+      console.log(inputArray[i]);
+      stepArray.push(inputArray[i]);
+    }
+    console.log(stepArray);
+  }
+  return stepArray;
+}
